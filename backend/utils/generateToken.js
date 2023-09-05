0dc08413c486c3e8e3b39bc8296977f92759f3a9
Abdullah Ali <eng.abdullah.ali@gmail.com>
2023-09-05 20:04:15 +1000

    changed json webtoken expiry to 2 min to test

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,7 @@ import jsw from 'jsonwebtoken';
 const generateToken = (res, userId)=>{
 
     const token = jsw.sign({userId}, process.env.JWT_SECRET,{
-        expiresIn:'30d'
+        expiresIn:120
     })
     res.cookie('jwt', token,{
     httpOnly:true,
